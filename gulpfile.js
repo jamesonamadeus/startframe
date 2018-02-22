acb9e29c1cd16bf309874fe21425c49ee5ca43ce
jamesonamadeus <jamesonamadeus@gmail.com>
2018-02-22 01:54:43 -0500

    add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// gulp sass
+gulp.task('sass', function(){
+    return gulp.src('src/css/scss/**/*.scss')
+      .pipe(sass()) // sass --> css
+      .pipe(gulp.dest('src/css'))
+});
+
+// watch
+gulp.task('watch', function(){
+    gulp.watch('src/css/scss/**/*.scss', ['sass']); 
+    // Other watchers
+})
