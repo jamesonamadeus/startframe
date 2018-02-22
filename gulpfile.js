bc49b78a2007aec207d391da704c93a89c0b3eea
jamesonamadeus <jamesonamadeus@gmail.com>
2018-02-22 02:07:00 -0500

    add in browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        },
+    })
+})
 
 // gulp sass
-gulp.task('sass', function(){
-    return gulp.src('src/css/scss/**/*.scss')
-      .pipe(sass()) // sass --> css
+gulp.task('sass', function() {
+    return gulp.src('src/css/scss/**/*.scss') // pulls all .scss files & its children
+      .pipe(sass()) // scss files --> css
       .pipe(gulp.dest('src/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
 });
 
 // watch
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync', 'sass'], function (){
     gulp.watch('src/css/scss/**/*.scss', ['sass']); 
     // Other watchers
-})
+});
